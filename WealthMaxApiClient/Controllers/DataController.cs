9c51aee9eab8a3074d85a929ed1d89a46031653c
Imran Khan <imranqau5373@gmail.com>
2020-09-10 21:21:35 +0500

    Created a new web api

---
+++ WealthMaxApiClient/Controllers/DataController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace WealthMaxApiClient.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    [EnableCors("AllowOrigin")]
+    public class DataController : ControllerBase
+    {
+
+        [HttpPost]
+        public ReponseObject Post([FromBody]Command commandObj)
+
+        {
+
+            var client = new RestClient("https://crm.wealthmax.co.uk/api/api/values?title="+commandObj.title +
+                "&firstname="+commandObj.firstname+
+                "&middlename=" + commandObj.middlename +
+                "&lastname=" + commandObj.lastname +
+                "&nexIBO=" + commandObj.nexIBO +
+                "&martial_status=" + commandObj.martial_status +
+                "&email=" + commandObj.email +
+                "&phone=" + commandObj.phone +
+                "&lead_source_id=" + commandObj.lead_source_id +
+                "&gender=Male" + commandObj.gender +
+                "&mortgage=" + commandObj.mortgage +
+                "&customer_isSmoker=" + commandObj.customer_isSmoker +
+                "&annual_income=" + commandObj.annual_income +
+                "&address1=Flat%201" + commandObj.address1 +
+                "&address2=153%20Sutton%20Lane" + commandObj.address2 +
+                "&city=Hounslow" + commandObj.city +
+                "&county=Middlesex" + commandObj.county +
+                "&country=United%20Kingdom" + commandObj.country +
+                "&postcode=TW3%204JW" + commandObj.postcode +
+                "&username=leads@nex-protect.co.uk" +
+                "&password=joshi123" +
+                "&apiKey=EuMysjk9YrW2rMYB4e5tt2Wxgs4NgkZV" +
+                "&selected_products=" + commandObj.selected_products +
+                "&date_of_birth=1991-12-13"+ commandObj.date_of_birth
+                );
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Postman-Token", "84dd05c8-8e34-40f7-b2aa-3cf7e7f16d2f");
+            request.AddHeader("cache-control", "no-cache");
+            IRestResponse response = client.Execute(request);
+            var result = response.Content;
+            var result = new ReponseObject() { }.ToString();
+            return JsonConvert.DeserializeObject<ReponseObject>(result);
+        }
+    }
+}
---
+++ WealthMaxApiClient/Models/ReponseObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WealthMaxApiClient
+{
+    public class ReponseObject
+    {
+        public int Status { get; set; }
+        public int CustomerId { get; set; }
+        public string[] Warnings { get; set; }
+        public string[] Errors { get; set; }
+
+    }
+    public class Command
+    {
+        public string title{ get; set; }
+        public string firstname { get; set; }
+        public string middlename { get; set; }
+        public string lastname { get; set; }
+        public string nexIBO { get; set; }
+        public string martial_status { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public int lead_source_id{ get; set; }
+        public string gender { get; set; }
+        public bool mortgage{ get; set; }
+        public bool customer_isSmoker{ get; set; }
+        public double annual_income{ get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string county { get; set; }
+        public string country { get; set; }
+        public string postcode { get; set; }
+        public string selected_products { get; set; }
+        public DateTime date_of_birth{ get; set; }
+    }
+}
